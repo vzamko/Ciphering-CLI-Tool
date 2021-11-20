7f19dbdabf8bd6e594540399c2fc8e8d335061f9
Valera Zamko <valera.zamko@jet.dev>
2021-11-20 09:39:20 +0200

    Improve code in index.test

--- index.test.js
+++ index.test.js
@@ -1,10 +1,11 @@
-const index = require('./index');
 const controller = require('./src/controller');
 
-jest.mock('./src/controller');
+jest.mock("./src/controller");
 
-describe('Index file:', () => {
-  test('must call run function', () => {
+describe("Index file:", () => {
+  test("must call run function", () => {
+    require('./index');
+
     expect(controller.run).toHaveBeenCalledTimes(1);
     expect(controller.run).toHaveBeenCalled();
   });
